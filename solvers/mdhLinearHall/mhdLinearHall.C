fa63e105d0665ebb889c9b748c572582c4d42b50
Dekempsy4 <felixtoft09@gmail.com>
2025-12-10 16:57:02 -0800

    investigating gmesh to generate mesh boundaries and groups

--- solvers/mdhLinearHall/mhdLinearHall.C
+++ solvers/mdhLinearHall/mhdLinearHall.C
@@ -86,34 +86,65 @@ int main(int argc, char *argv[])
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
     Info<< "\nStarting time loop\n" << endl;
+    Info<< "Debug: entering initial Elmer coupling before first time step" << nl << endl;
 
     // Send fields to Elmer
+    Info<< "Debug: creating Elmer sender and sending initial elcond field" << nl << endl;
     Elmer<fvMesh> sending(mesh,1); // 1=send, -1=receive
     sending.sendStatus(1); // 1=ok, 0=lastIter, -1=error
     elcond = elcond_melt;
     sending.sendScalar(elcond);
 
+    if (Pstream::master())
+    {
+        Info<< "Initial Elmer send (elcond) completed" << nl << endl;
+    }
+
     // Receive fields from Elmer
+    Info<< "Debug: creating Elmer receiver and waiting for initial JxB/JH" << nl << endl;
     Elmer<fvMesh> receiving(mesh,-1); // 1=send, -1=receive
     receiving.sendStatus(1); // 1=ok, 0=lastIter, -1=error
     receiving.recvVector(JxB_recv);
     receiving.recvScalar(JH_recv);
 
+    if (Pstream::master())
+    {
+        Info<< "Initial Elmer receive (JxB, JH) completed" << nl << endl;
+    }
+
     while (runTime.run())
     {
+        Info<< "Debug: top of time-step while(runTime.run()), time = "
+            << runTime.timeName() << nl << endl;
+
         JxB = JxB_recv;
         JH = JH_recv;
 
+        if (Pstream::master())
+        {
+            Info<< "Entering time step, current time = " << runTime.timeName()
+                << ", maxRelDiff trigger = " << maxRelDiff << nl << endl;
+        }
+
+        Info<< "Debug: about to read time controls" << nl << endl;
         #include "readTimeControls.H"
+        Info<< "Debug: finished readTimeControls.H, current deltaT = "
+            << runTime.deltaTValue() << nl << endl;
 
         if (LTS)
         {
+            Info<< "Debug: using local time stepping, entering setRDeltaT.H" << nl << endl;
             #include "setRDeltaT.H"
+            Info<< "Debug: finished setRDeltaT.H" << nl << endl;
         }
         else
         {
+            Info<< "Debug: computing Courant number" << nl << endl;
             #include "CourantNo.H"
+            Info<< "Debug: finished CourantNo.H, entering setDeltaT.H" << nl << endl;
             #include "setDeltaT.H"
+            Info<< "Debug: finished setDeltaT.H, new deltaT = "
+                << runTime.deltaTValue() << nl << endl;
         }
 
         runTime++;
@@ -121,25 +152,35 @@ int main(int argc, char *argv[])
         Info<< "Time = " << runTime.timeName() << nl << endl;
 
         // --- Pressure-velocity PIMPLE corrector loop
+        Info<< "Debug: entering PIMPLE loop for this time step" << nl << endl;
         while (pimple.loop())
         {
+            Info<< "Debug:  PIMPLE outer iteration, calling laminarTransport.correct()" << nl << endl;
             laminarTransport.correct();
 
+            Info<< "Debug:  about to assemble/solve UEqn (UEqn.H)" << nl << endl;
             #include "UEqn.H"
+            Info<< "Debug:  finished UEqn.H" << nl << endl;
 
+            Info<< "Debug:  about to assemble/solve TEqn (TEqn.H)" << nl << endl;
             #include "TEqn.H"
+            Info<< "Debug:  finished TEqn.H" << nl << endl;
 
             // --- Pressure corrector loop
+            Info<< "Debug:  entering pressure corrector loop (pEqn.H)" << nl << endl;
             while (pimple.correct())
             {
                 #include "pEqn.H"
             }
+            Info<< "Debug:  finished pressure corrector loop" << nl << endl;
 
             if (pimple.turbCorr())
             {
+                Info<< "Debug:  turbulence correction enabled, calling turbulence->correct()" << nl << endl;
                 turbulence->correct();
             }
         }
+        Info<< "Debug: leaving PIMPLE loop for this time step" << nl << endl;
 
         runTime.write();
 
@@ -162,6 +203,11 @@ int main(int argc, char *argv[])
         scalar maxRelDiff_local =
             (max(mag(U_old - U)/(average(mag(U)) + smallU))).value();
 
+        if (Pstream::master())
+        {
+            Info<< "Computed maxRelDiff_local = " << maxRelDiff_local << nl << endl;
+        }
+
         bool doElmer = false;
         if
         (
@@ -170,10 +216,22 @@ int main(int argc, char *argv[])
         )
         {
             doElmer = true;
+
+            if (Pstream::master())
+            {
+                Info<< "Elmer update triggered at time = " << runTime.timeName()
+                    << " (maxRelDiff_local = " << maxRelDiff_local << ")" << nl << endl;
+            }
         }
 
         if (doElmer || !runTime.run())
         {
+            if (Pstream::master())
+            {
+                Info<< "Performing Elmer coupling step at time = "
+                    << runTime.timeName() << nl << endl;
+            }
+
             U_old = U;
 
             // Send fields to Elmer
@@ -185,6 +243,12 @@ int main(int argc, char *argv[])
             receiving.sendStatus(runTime.run());
             receiving.recvVector(JxB_recv);
             receiving.recvScalar(JH_recv);
+
+            if (Pstream::master())
+            {
+                Info<< "Elmer coupling step completed at time = "
+                    << runTime.timeName() << nl << endl;
+            }
         }
     }
 
